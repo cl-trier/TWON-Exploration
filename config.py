255ac2edb29be4134cf27696edcbd2bc0a5c2301
simon-muenker <simon.muenker@googlemail.com>
2024-05-13 12:01:04 +0200

    add processing/description notebooks

---
+++ config.py
@@ -0,0 +1,31 @@
+import glob
+import pathlib
+import typing
+
+import pydantic
+
+DATA_DIR: pathlib.Path = pathlib.Path('./data')
+REPORT_DIR: pathlib.Path = pathlib.Path('./report')
+
+
+class Config(pydantic.BaseModel):
+    experiment: str = "2024-05--Simulation"
+
+    raw_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
+    processed_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
+
+    report_dir: pathlib.Path = None
+
+    def model_post_init(self, __context: typing.Any) -> None:
+        self.raw_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/raw/*')
+        self.processed_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/processed/*')
+
+        self.report_dir = REPORT_DIR / self.experiment
+        pathlib.Path(self.report_dir).mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def load_data_dir(path: str) -> typing.Dict[str, pathlib.Path]:
+        return {
+            (file_obj := pathlib.Path(file_path)).stem: file_obj
+            for file_path in glob.glob(path)
+        }
