89fcf785de271da49cfc75feaf660edd3b79cc0c
simon-muenker <simon.muenker@googlemail.com>
2024-05-13 12:35:02 +0200

    add aggregation

--- config.py
+++ config.py
@@ -13,15 +13,20 @@ class Config(pydantic.BaseModel):
 
     raw_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
     processed_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
+    final_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
 
+    data_dir: pathlib.Path = None
     report_dir: pathlib.Path = None
 
     def model_post_init(self, __context: typing.Any) -> None:
-        self.raw_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/raw/*')
-        self.processed_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/processed/*')
-
+        self.data_dir = DATA_DIR / self.experiment
         self.report_dir = REPORT_DIR / self.experiment
         pathlib.Path(self.report_dir).mkdir(parents=True, exist_ok=True)
+
+        self.raw_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/raw/*')
+        self.processed_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/processed/*')
+        self.final_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/final/*')
+
 
     @staticmethod
     def load_data_dir(path: str) -> typing.Dict[str, pathlib.Path]:
