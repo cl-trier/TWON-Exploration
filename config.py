48375a3302c2dbe86ccfd97a2b6dd2d4ed853d17
Simon Münker <muenker@uni-trier.de>
2024-06-25 14:20:03 +0200

    prepare prompt base coding for dicourse metrics

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ REPORT_DIR: pathlib.Path = pathlib.Path('./report')
 class Config(pydantic.BaseModel):
     experiment: str = "2024-05--Simulation"
 
-    prompt_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
+    prompt_classify_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
 
     raw_data_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
     processed_data_files: typing.Dict[str, pathlib.Path] = pydantic.Field(default_factory=lambda: dict())
@@ -26,7 +26,7 @@ class Config(pydantic.BaseModel):
         self.report_dir = REPORT_DIR / self.experiment
         pathlib.Path(self.report_dir).mkdir(parents=True, exist_ok=True)
 
-        self.prompt_files = Config.load_data_dir(f'{DATA_DIR}/prompts/*')
+        self.prompt_classify_files = Config.load_data_dir(f'{DATA_DIR}/prompts_classify/*')
 
         self.raw_data_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/raw/*')
         self.processed_data_files = Config.load_data_dir(f'{DATA_DIR}/{self.experiment}/processed/*')
